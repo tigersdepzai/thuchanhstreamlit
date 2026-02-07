fcc3fd38fb266456784af56265f2973e46f618f1
tổng tài peesi <tigersdepzai@gmail.com>
2026-02-07 20:15:26 +0700

    cập nhật lại bài của tôi

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import streamlit as st
-st.set_pade_config(page_title="miêu tả ca sĩ yêu thích", page_icon=":cat:", layout="wide")
+st.set_page_config(page_title="miêu tả ca sĩ yêu thích", page_icon=":cat:", layout="wide")
 with st.sidebar:
     st.title("miêu tả ca sĩ yêu thích")
     st.image("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTzcvnVu-2kmDzAmzho1Ooe1ZrgRUS2xNB7Ug&s")
@@ -8,47 +8,54 @@ with st.sidebar:
     Họ Và Tên:Trịnh Trần Phương Tuấn
     chi tiết về nhạc sĩ:tên thật là Trịnh Trần Phương Tuấn (sinh ngày 12/04/1997 tại Bến Tre), là ca sĩ, nhạc sĩ và rapper nổi tiếng của Việt Nam""")
 st.title("Bài hát yêu thích của tôi:")
-st.audio("https://www.nhaccuatui.com/song/44Chq6pt3FCA?source=app")
+st.audio("https://www.google.com/search?q=nh%E1%BA%A1c+v%E1%BB%81+b%C3%AAn+anh&sca_esv=b21dda8be8fa643d&biw=1290&bih=725&sxsrf=ANbL-n5W0-AoUPFGDSlGeWtzRGYqFbbnbA%3A1770469932261&ei=LDqHafbVD7GOseMPxqWN-A8&ved=0ahUKEwj2m5e7useSAxUxR2wGHcZSA_8Q4dUDCBM&uact=5&oq=nh%E1%BA%A1c+v%E1%BB%81+b%C3%AAn+anh&gs_lp=Egxnd3Mtd2l6LXNlcnAiFG5o4bqhYyB24buBIGLDqm4gYW5oMgUQABiABDIGEAAYFhgeMgYQABgWGB4yBhAAGBYYHjIGEAAYFhgeMgYQABgWGB4yBhAAGBYYHjIGEAAYFhgeMgYQABgWGB4yBhAAGBYYHkiTOlDMBFiqNHAGeAGQAQaYAcoBoAHCGaoBBjI4LjYuMbgBA8gBAPgBAZgCHaAC_hHCAgoQABiwAxjWBBhHwgIEECMYJ8ICBRAAGO8FwgIIEAAYgAQYogTCAggQABiABBjLAcICBRAuGIAEwgIIEAAYgAQYsQPCAgoQIxiABBgnGIoFwgIOEAAYgAQYsQMYgwEYigXCAgQQABgDwgIUEC4YgAQYlwUY3AQY3gQY4ATYAQGYAwCIBgGQBgi6BgYIARABGBSSBwQyMC45oAfvhgKyBwQxNC45uAfSEcIHBjItMjIuN8gHxAGACAA&sclient=gws-wiz-serp#fpstate=ive&vld=cid:ae81ff7a,vid:fArpx8TRWU8,st:0")
 st.write("""
-    lời bài hát:
-    Đã có lúc ấm áp đôi tay, cùng nhau nhìn lên trời cao
-Đến phút cuối em bước ra đi, làm sao để giữ được em
-Tìm hoài hình bóng lúc ấy, tìm hoài cảm giác bối rối, lòng này anh đã cố nói đừng đi
-Buổi chiều hôm ấy khuất lối, chìm vào bóng tối nhứt nhói, lệ nhòe hoen mắt chẳng thể ngừng rơi
-Ở nơi đó em có vui không, người bên em có giống anh không?
-Họ có biết những lúc em đau, cần chia sớt với những u sầu
-Về bên anh gió lộng đồi hoang, ở bên anh yên giấc mơ màng
-Ngồi đây nghe tiếng lòng thở than, chờ mong ai hơi ấm nhẹ nhàng
-Dù rằng một giây nữa thôi, ghì chặt bờ môi xiết ôm, đừng vội vàng quên
-Rời xa anh mãi
-Xin đừng đi, anh cần em nhớ em
-Lá vàng rơi xuân hạ tới, đông và thu nhớ em
-
+    lời bài hát: về bên anh - Trịnh Trần Phương Tuấn
+   Đã có lúc ấm áp đôi tay, cùng nhau nhìn lên trời cao
+Đến phút cuối em bước ra đi, làm sao để giữ được em?
+Tìm hoài hình bóng lúc ấy, tìm hoài cảm giác bối rối
+Lòng này anh đã cố nói "đừng đi"
+Buổi chiều hôm ấy khuất lối, chìm vào bóng tối nhức nhói
+Lệ nhòe hoen mắt, chẳng thể ngừng rơi
+Ở nơi đó em có vui không?
+Người bên em có giống anh không?
+Họ có biết những lúc em đau
+Cần chia sớt với những u sầu
+Về bên anh gió lộng đồi hoang
+Ở bên anh yên giấc mơ màng
+Ngồi đây nghe tiếng lòng thở than
+Chờ mong ai hơi ấm nhẹ nhàng
+Dù rằng một giây nữa thôi, ghị chặt bờ môi xiết ôm
+Đừng vội vàng quên rời xa anh mãi
+Xin đừng đi, anh cần em, nhớ em
+Lá vàng rơi, xuân hạ tới, đông và thu nhớ em
 Anh có thể vẽ em thật kiêu sa, nét ngọc ngà trên áo còn thêu hoa
 Phút chạnh lòng anh cứ tưởng là khi xa, sẽ không buồn với những thứ mình đi qua
-Em ơi! Thanh xuân này ngắn ngủi
-Đôi giấy nhỏ làm sao viết thành văn, tơ vò còn vương lại khe núi, ôm cả bầu trời niên thiếu có đành chăng?
+Em ơi (em ơi), thanh xuân này ngắn ngủi, đôi giấy nhỏ làm sao viết thành văn
+Tơ vò còn vương lại khe núi, ôm cả bầu trời niên thiếu có đành chăng?
 Em biết không mùa xuân chẳng trọn vẹn, tim lạc đường khi ta chẳng thấy nhau
-Nhìn trăng kia vàng còn treo trước đầu ngõ, giờ em đi rồi mùa hạ hôm ấy đâu?
-Anh chẳng ước, mình như là cánh chim, bay giữa muôn trùng đất mẹ này bao la
-Anh muốn được nghe giọng em nói muốn thấy em cười bình yên, chẳng sao cả, nghe anh
-
-
-Về bên anh nhé em! Cầm tay anh nhé em, cùng bên nhau và sưởi ấm đêm đông
-Buốt giá
+Nhìn trăng kia vàng còn treo trước đầu ngỏ, giờ em đi rồi, mùa hạ hôm ấy đâu?
+Anh chẳng ước mình như là cánh chim bay giữa muôn trùng đất mẹ này bao la
+Anh muốn được nghe giọng em nói, muốn thấy em cười bình yên chẳng sao cả (nghe anh)
+Về bên anh nhé em, cầm tay anh nhé em
+Cùng bên nhau và sưởi ấm đêm đông buốt giá
 Lòng anh thương nhớ em, chìm vào trong giấc mơ
-Để con tim, một lần nữa kêu tên
-
-Vì đôi lúc anh thấy em giận anh quá nên thôi, hình bóng ấy có thể phai nhòa
-Nhưng chẳng xa xôi
-Cành phượng vĩ kia đã đâm chồi thay lá đơm bông, thì thôi nhé em cứ ở lại giây phút
-Ta mong đưa đôi tay nhìn lên trời, hương thơm kia tựa mây ngàn
+Để con tim một lần nữa kêu tên
+Vì đôi lúc anh thấy em giận anh quá nên thôi
+Hình bóng ấy có thể phai nhòa nhưng chẳng xa xôi
+Cành phượng vĩ kia đã đâm chồi, thay lá, đơm bông
+Thì thôi nhé em cứ ở lại giây phút ta mong
+Đưa đôi tay nhìn lên trời, hương thơm kia tựa mây ngàn
 Anh muốn ôm bờ vai này sao chẳng thấy
-Chợt bồi hồi vì lòng chưa quên, đoạn đường buồn vội vàng không tên
-Tiếc nuối ấy cứ thế vẫn mãi khắc sâu trên hàng mi, và rồi nhận ra yêu thương bên nhau dần vỡ nát
-Giọt lệ anh đã cố giấu bước tiếp để nhìn em bước đi
+Chợt bồi hồi vì lòng chưa quên
+Đoạn đường buồn vội vàng không tên em hỡi
+Tiếc nuối ấy cứ thế vẫn mãi khắc sâu trên hàng mi
+Và rồi nhận ra yêu thương bên nhau dần vỡ nát
+Giọt lệ anh đã cố dấu bước tiếp để nhìn em bước đi
 Nơi đó anh nhớ em nhiều
-Về bên anh gió lộng đồi hoang, ở bên anh yên giấc mơ màng
-Ngồi đây nghe tiếng lòng thởh than, chờ mong ai hơi ấm nhẹ nhàng""")
+Về bên anh gió lộng đồi hoang
+Ở bên anh yên giấc mơ màng
+Ngồi đây nghe tiếng lòng thở than
+Chờ mong ai hơi ấm nhẹ nhàng""")
 st.title("MV yêu thích của tôi:")
 st.video("https://www.youtube.com/watch?v=Hpjd3dNXQy0&list=RDHpjd3dNXQy0&start_radio=1")
